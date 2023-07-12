1d70aa221ef8435b4f3ab047f3d5dd1aa2925f87
Joey Perrello <98714211+jperrello@users.noreply.github.com>
2023-07-12 11:28:10 +0100

    Update script.js
    added feature to scale graph no matter the input size

--- script.js
+++ script.js
@@ -1,3 +1,54 @@
+// layout function for creating layouts of the graph
+function createBarChartLayout(headers, values) {
+  var maxValue = Math.max(...values);
+  var numCategories = headers.length - 1;
+  var barGroupWidth = Math.min(0.8, 2.0 / numCategories);
+
+  // Calculate the desired width and height based on the values and categories
+  var desiredWidth = Math.max(800, 80 * numCategories);
+  var desiredHeight = Math.max(600, 40 * values.length);
+
+  return {
+    title: 'Number of Users in Each Game',
+    barmode: 'stack',
+    bargap: 0.8,
+    bargroupwidth: barGroupWidth,
+    autosize: true, // Enable autosizing
+    //call above variables
+    width: desiredWidth, 
+    height: desiredHeight, 
+    lines: false,
+    color: 'rgba(31, 119, 180, 0.8)',
+    xaxis: {
+      title: headers[1],
+      range: [0, maxValue + 100],
+      fixedrange: true,
+      zeroline: false,
+      showgrid: false,
+    },
+    yaxis: {
+      title: {
+        text: headers[0],
+        standoff: 30,
+      },
+      automargin: true,
+      showgrid: false,
+      gridwidth: 3,
+      zeroline: false,
+      tickfont: {
+        size: 10,
+      },
+    },
+    margin: {
+      1: 150,
+    },
+  };
+}
+
+
+
+
+
 function handleFile() {
   // read the csv file after taking it as input
   var fileInput = document.getElementById("csvFileInput");
@@ -154,39 +205,7 @@ for (var xHeader in dataByHeaders) {
   colorIndex++;
 }
 
-var barLayout = {
-  title: 'Plotly Practice',
-  barmode: 'stack',
-  bargap: 0.8,
-  bargroupwidth: 2.0,
-  autosize: false,
-  width: 1200,
-  height: 1000,
-  lines: false,
-  color: 'rgba(31, 119, 180, 0.8)',
-  xaxis: {
-    title: headers[1],
-    Range: [0, 2200],
-    zeroline:false,
-    showgrid: false
-  },
-  yaxis: {
-    title: {
-      text: headers[0],
-      standoff: 30,
-    },
-    automargin: true,
-    showgrid: false,
-    gridwidth: 3,
-    zeroline: false,
-    tickfont: {
-      size: 10
-    }
-  },
-  margin: {
-    1: 150 
-  }
-};
+var barLayout = createBarChartLayout(headers, values);
 
 
 Plotly.newPlot('barChartContainer', barData, barLayout).then(function() {
@@ -228,94 +247,142 @@ Plotly.newPlot('barChartContainer', barData, barLayout).then(function() {
   reader.readAsText(file);
 }
 
-//**************************** */
-//      Practice Graphs        */
-//**************************** */
-function createLine() {
+// smaller graph practice
+function handleSmallFile() {
+  // read the csv file after taking it as input
+  var fileInput = document.getElementById("smallCsvFileInput");
+  var file = fileInput.files[0];
+  var reader = new FileReader();
 
-  var trace1 = {
+  reader.onload = function (e) {
+    var contents = e.target.result;
+    var lines = contents.split("\n");
+    var dataByHeaders = {};
 
-    x: [1,2,3,4,5,6,7,8,9,10,11,12],
+// get header values
+    var headers = lines[0].split(",");
 
-    y: [60, 45, 50, 32, 27, 12, 43, 24, 28, 38, 15, 9],
-
-    mode: 'lines',
-
-    name: 'User 207 Engagement'
-
-  };
-
-
-  var trace2 = {
-
-    x: [1,2,3,4,5,6,7,8,9,10,11,12],
-
-    y: [75, 60, 55, 63, 48, 34, 38, 29, 25, 18, 24, 12],
-
-    mode: 'lines+markers',
-
-    name: 'Average User Engagement'
-
-  };
-
-
-  var data = [trace1, trace2];
-
-
-  var layout = {
-
-    title: 'User Engagement Over a Year',
-
-    xaxis: {
-
-      title: 'Months'
-
-    },
-
-    yaxis: {
-
-      title: 'Minutes Played Monthly'
-
+// Initialize dataByHeaders object (sorting data by headers)
+    for (var i = 1; i < headers.length; i++) {
+      dataByHeaders[headers[i]] = {};
     }
 
+// parse through each line of data
+    for (var i = 1; i < lines.length; i++) {
+      var line = lines[i].split(",");
+
+  // Assign values to the headers
+      for (var j = 1; j < headers.length; j++) {
+        var xHeader = headers[j];
+        var yHeader = line[0];
+        var value = parseInt(line[j]);
+
+        if (!isNaN(value)) {
+          if (!dataByHeaders[xHeader].hasOwnProperty(yHeader)) {
+            dataByHeaders[xHeader][yHeader] = 0;
+          }
+
+          dataByHeaders[xHeader][yHeader] += value;
+        }
+      }
+    }
+    var totalValue = 0;
+    for (var xHeader in dataByHeaders) {
+      console.log(xHeader + ":");
+      var yHeaders = Object.keys(dataByHeaders[xHeader]);
+
+      for (var yHeader of yHeaders) {
+        var value = dataByHeaders[xHeader][yHeader];
+        console.log(yHeader + "\nValue: " + value);
+        totalValue += value;
+      }
+
+      console.log("------");
+      console.log("Total Value: " + totalValue);
+    }
+var barData = [];
+
+var colorIndex = 0;
+// parse through data
+for (var xHeader in dataByHeaders) {
+  var yHeaders = Object.keys(dataByHeaders[xHeader]);
+  var values = [];
+  for (var xHeader in dataByHeaders) {
+    var yHeaders = Object.keys(dataByHeaders[xHeader]);
+    var values = [];
+    for (var yHeader of yHeaders) {
+      var value = dataByHeaders[xHeader][yHeader];
+      values.push(value);
+    }
+    //sort data, then flip it since smallest comes before largest when sorting
+    var sortedIndices = values.map(function (_, index) {
+      return index;
+    }).sort(function (a, b) {
+      return values[b] - values[a];
+    });
+    yHeaders = sortedIndices.map(function (index) {
+      return yHeaders[index];
+    });
+    values = sortedIndices.map(function (index) {
+      return values[index];
+    });
+  }
+  yHeaders.reverse();
+  values.reverse();
+
+  var barTrace = {
+    y: yHeaders,
+    x: values,
+    width: [0.7, 0.7, 0.7, 0.7, 0.7, 0.7, 0.7, 0.7, 0.7, 0.7 
+       ],
+    type: 'bar',
+    name: xHeader,
+    orientation: 'h',
+    text: values.map(String),
+    textposition: 'auto',
+    textfont_size: 20,
+    textposition: 'outside',
+    cliponaxis: false,
+    
   };
-
-
-  Plotly.newPlot('myDiv2', data, layout);
+  barData.push(barTrace);
+  colorIndex++;
 }
 
-function createGeo() {
+var barLayout = createBarChartLayout(headers, values);
 
-var data = [{
-  type: 'scattergeo',
-  mode: 'markers',
-  locations: ['London', 'FRA', 'DEU', 'RUS', 'ESP', 'Ireland', 'Italy'],
-  marker: {
-      size: [60, 20, 30, 15, 10, 40, 50],
-      color: [10, 20, 40, 50, 60, 70, 80],
-      cmin: 0,
-      cmax: 80,
-      colorscale: 'Greens',
-      colorbar: {
-          title: 'Number of installs',
-          showticksuffix: 'last'
-      },
-      line: {
-          color: 'black'
-      }
-  },
-  name: 'europe data'
-}];
+Plotly.newPlot('smallBarChartContainer', barData, barLayout).then(function() {
+  var barChart = document.getElementById('smallBarChartContainer');
+  var colors = Array.from({ length: yHeaders.length }, () => 'rgba(31, 119, 180, 0.8)'); // Blue color
+  
 
-var layout = {
-  'geo': {
-      'scope': 'europe',
-      'resolution': 100
-  },
-  height: 900,
-  width: 900
+  barChart.on('plotly_click', function(data) {
+    var clickedIndex = data.points[0].pointIndex;
 
-};
+    // Reset colors to blue for all bars
+    colors = Array.from({ length: yHeaders.length }, () => 'rgba(31, 119, 180, 0.8)');
 
-Plotly.newPlot('myDiv3', data, layout);
+    // Set the clicked bar color to green
+    colors[clickedIndex] = 'green';
+
+    var update = {
+      marker: { color: colors }
+    };
+
+    Plotly.update('smallBarChartContainer', update);
+    
+    // showcase the clicked bar's information onto the webpage itself
+    var clickedBarData = data.points[0]; // Get the clicked bar's data directly
+    var clickedBarName = clickedBarData.label;
+    var clickedBarValue = clickedBarData.x;
+    console.log(clickedBarData)
+
+
+    var statement = "Selected Information: " + clickedBarName + "<br>Value: " + clickedBarValue;
+    document.getElementById('outputSelectSmallInfo').innerHTML = statement;
+    document.getElementById('outputSelectSmallInfo').style.border = "2px solid green";
+  });
+});
+  }
+  reader.readAsText(file);
 }
