ac99ece8578643a2652e7d1a2bcd8d521afeccc5
Joey Perrello <98714211+jperrello@users.noreply.github.com>
2023-07-13 11:34:36 +0100

    Update script.js
    fixed issue so when a bar is clicked again after being clicked, color is reverted to blue and all of the information displayed is deleted. also made it so handleFile is the only function needed to be called so handleSmallFile could be deleted.

--- script.js
+++ script.js
@@ -1,27 +1,26 @@
-// layout function for creating layouts of the graph
+//seperate function for handling the layout so all 
+//formats are the same no matter the num of entries
 function createBarChartLayout(headers, values) {
   var maxValue = Math.max(...values);
   var numCategories = headers.length - 1;
-  var barGroupWidth = Math.min(0.8, 2.0 / numCategories);
 
-  // Calculate the desired width and height based on the values and categories
+  // Calculate the desired width and height based on the number of categories
   var desiredWidth = Math.max(800, 80 * numCategories);
   var desiredHeight = Math.max(600, 40 * values.length);
 
+  //basic layout formatting
   return {
     title: 'Number of Users in Each Game',
     barmode: 'stack',
     bargap: 0.8,
-    bargroupwidth: barGroupWidth,
-    autosize: true, // Enable autosizing
-    //call above variables
-    width: desiredWidth, 
+    bargroupwidth: 0.7,
+    autosize: true, 
+    width: desiredWidth,
     height: desiredHeight, 
     lines: false,
     color: 'rgba(31, 119, 180, 0.8)',
     xaxis: {
       title: headers[1],
-      range: [0, maxValue + 100],
       fixedrange: true,
       zeroline: false,
       showgrid: false,
@@ -44,41 +43,42 @@ function createBarChartLayout(headers, values) {
     },
   };
 }
-
-
-
-
-
-function handleFile() {
-  // read the csv file after taking it as input
-  var fileInput = document.getElementById("csvFileInput");
+//one handleFile function for all chart inputs
+//three parameters to pass different ids for different chart inputs
+function handleFile(fileInputId, chartContainerId, outputInfoId) {
+  var fileInput = document.getElementById(fileInputId);
+  var chartContainer = document.getElementById(chartContainerId);
+  var outputInfo = document.getElementById(outputInfoId);
   var file = fileInput.files[0];
   var reader = new FileReader();
 
+  //start reading csv file
   reader.onload = function (e) {
     var contents = e.target.result;
     var lines = contents.split("\n");
     var dataByHeaders = {};
 
-// get header values
+    // get header values
     var headers = lines[0].split(",");
 
-// Initialize dataByHeaders object (sorting data by headers)
+    // Initialize dataByHeaders object (sorting data by headers)
     for (var i = 1; i < headers.length; i++) {
       dataByHeaders[headers[i]] = {};
     }
 
-// parse through each line of data
+    // parse through each line of data
     for (var i = 1; i < lines.length; i++) {
       var line = lines[i].split(",");
 
-  // Assign values to the headers
+      // Assign values to the headers
       for (var j = 1; j < headers.length; j++) {
         var xHeader = headers[j];
         var yHeader = line[0];
         var value = parseInt(line[j]);
 
+        //make sure that only numeric values are being considered
         if (!isNaN(value)) {
+          //if a y value doesnt exist, initialize to zero
           if (!dataByHeaders[xHeader].hasOwnProperty(yHeader)) {
             dataByHeaders[xHeader][yHeader] = 0;
           }
@@ -88,8 +88,9 @@ function handleFile() {
       }
     }
 
-// Print Events to console and calculate total value
-// of all values added together
+    //log all values and inputs to the console (primarily for testing)
+    //calculate total value for pie chart funtion (now removed), or
+    //in case we would like to use the total value in the future
     var totalValue = 0;
     for (var xHeader in dataByHeaders) {
       console.log(xHeader + ":");
@@ -105,284 +106,106 @@ function handleFile() {
       console.log("Total Value: " + totalValue);
     }
 
-//Pie Chart
-    var pieData = [];
+    //start creating bar graph
+    var barData = [];
+
+    var colorIndex = 0;
+    // parse through data
     for (var xHeader in dataByHeaders) {
       var yHeaders = Object.keys(dataByHeaders[xHeader]);
+      var values = [];
 
-      var sectionSizes = [];
-      var labels = [];
-      var ogValues = [];
       for (var yHeader of yHeaders) {
         var value = dataByHeaders[xHeader][yHeader];
-        var sectionSize = value / totalValue;
-
-        sectionSizes.push(sectionSize);
-        labels.push(yHeader);
-        ogValues.push(value);
+        values.push(value);
       }
 
-      var dataPoint = {
-        values: sectionSizes,
-        labels: labels,
-        type: 'pie',
-        hole: 0.45,
-        hoverinfo:'label+text+percent',
-        text: ogValues.map(String)
+      // sort data, then flip it since smallest comes before largest when sorting
+      // this way the largest elements will be on top of the bar chart, and smallest
+      // on the bottom
+      var sortedIndices = values.map(function (_, index) {
+        return index;
+      }).sort(function (a, b) {
+        return values[b] - values[a];
+      });
+
+      yHeaders = sortedIndices.map(function (index) {
+        return yHeaders[index];
+      });
+
+      values = sortedIndices.map(function (index) {
+        return values[index];
+      });
+
+      yHeaders.reverse();
+      values.reverse();
+
+      //trace for bargraph
+      var barTrace = {
+        y: yHeaders,
+        x: values,
+        width: Array(values.length).fill(0.7), // Set a fixed width for all bars
+        type: 'bar',
+        name: xHeader,
+        orientation: 'h',
+        //number values displayed next to bar
+        text: values.map(String),
+        textposition: 'auto',
+        textfont_size: 20,
+        textposition: 'outside',
+        cliponaxis: false,
       };
 
-      pieData.push(dataPoint);
+      barData.push(barTrace);
+      colorIndex++;
     }
+    
+    var barLayout = createBarChartLayout(headers, values);
 
-    var layout = {
-      showlegend: true,
-      width: 900,
-      height: 800,
-      annotations: [
-        {
-          text: 'Number of Users in Each Game',
-          x: 0.5,
-          y: 0.5,
-          showarrow: false,
-          font: {
-            size: 14,
-            color: 'black'
-          }
+    //actually create graph, but then start adding the clicking bar fucnction
+    Plotly.newPlot(chartContainer, barData, barLayout).then(function () {
+      var colors = Array.from({ length: yHeaders.length }, () => 'rgba(31, 119, 180, 0.8)');
+    
+      chartContainer.on('plotly_click', function (data) {
+        var clickedIndex = data.points[0].pointIndex;
+    
+        // Set the clicked bar color to green or revert back to blue if already clicked
+        if (colors[clickedIndex] === 'green') {
+          colors[clickedIndex] = 'rgba(31, 119, 180, 0.8)';
+        } else {
+          colors[clickedIndex] = 'green';
         }
-      ]
-    };
-
-    Plotly.newPlot('pieChartContainer', pieData, layout);
-
-//bar graph
-var barData = [];
-
-var colorIndex = 0;
-// parse through data
-for (var xHeader in dataByHeaders) {
-  var yHeaders = Object.keys(dataByHeaders[xHeader]);
-  var values = [];
-  for (var xHeader in dataByHeaders) {
-    var yHeaders = Object.keys(dataByHeaders[xHeader]);
-    var values = [];
-    for (var yHeader of yHeaders) {
-      var value = dataByHeaders[xHeader][yHeader];
-      values.push(value);
-    }
-    //sort data, then flip it since smallest comes before largest when sorting
-    var sortedIndices = values.map(function (_, index) {
-      return index;
-    }).sort(function (a, b) {
-      return values[b] - values[a];
+        //update color array to include clickedIndex's color
+        var update = {
+          marker: { color: colors }
+        };
+    
+        Plotly.update(chartContainer, update);
+    
+        if (colors[clickedIndex] === 'green') {
+          // Showcase the clicked bar's information onto the webpage itself
+          var clickedBarData = data.points[0]; // Get the clicked bar's data directly
+          var clickedBarName = clickedBarData.label;
+          var clickedBarValue = clickedBarData.x;
+          console.log(clickedBarData);
+    
+          var statement = "Selected Information: " + clickedBarName + "<br>Value: " + clickedBarValue;
+          outputInfo.innerHTML = statement;
+          outputInfo.style.border = "2px solid green";
+        } else { //if the bar has already been clicked, delete info showcased on webpage
+          outputInfo.innerHTML = "";
+          outputInfo.style.border = "none";
+        }
+      });
+    
+      var update = {
+        marker: { color: colors }
+      };
+    
+      Plotly.update(chartContainer, update);
     });
-    yHeaders = sortedIndices.map(function (index) {
-      return yHeaders[index];
-    });
-    values = sortedIndices.map(function (index) {
-      return values[index];
-    });
-  }
-  yHeaders.reverse();
-  values.reverse();
-
-  var barTrace = {
-    y: yHeaders,
-    x: values,
-    width: [0.7, 0.7, 0.7, 0.7, 0.7, 0.7, 0.7, 0.7, 0.7, 0.7, 0.7, 
-      0.7, 0.7, 0.7, 0.7, 0.7, 0.7, 0.7, 0.7, 0.7, 0.7, 
-      0.7, 0.7, 0.7, 0.7, 0.7, 0.7, 0.7, 0.7, 0.7, 0.7, 0.7, 0.7 ],
-    type: 'bar',
-    name: xHeader,
-    orientation: 'h',
-    text: values.map(String),
-    textposition: 'auto',
-    textfont_size: 20,
-    textposition: 'outside',
-    cliponaxis: false,
     
   };
-  barData.push(barTrace);
-  colorIndex++;
-}
 
-var barLayout = createBarChartLayout(headers, values);
-
-
-Plotly.newPlot('barChartContainer', barData, barLayout).then(function() {
-  var barChart = document.getElementById('barChartContainer');
-  var colors = Array.from({ length: yHeaders.length }, () => 'rgba(31, 119, 180, 0.8)'); // Blue color
-
-  barChart.on('plotly_click', function(data) {
-    var clickedIndex = data.points[0].pointIndex;
-
-    // Reset colors to blue for all bars
-    colors = Array.from({ length: yHeaders.length }, () => 'rgba(31, 119, 180, 0.8)');
-
-    // Set the clicked bar color to green
-    colors[clickedIndex] = 'green';
-
-    var update = {
-      marker: { color: colors }
-    };
-
-    Plotly.update('barChartContainer', update);
-    
-    // showcase the clicked bar's information onto the webpage itself
-    var clickedBarData = data.points[0]; // Get the clicked bar's data directly
-    var clickedBarName = clickedBarData.label;
-    var clickedBarValue = clickedBarData.x;
-    console.log(clickedBarData)
-
-
-    var statement = "Selected Information: " + clickedBarName + "<br>Value: " + clickedBarValue;
-    document.getElementById('outputSelectInfo').innerHTML = statement;
-    document.getElementById('outputSelectInfo').style.border = "2px solid green";
-  });
-});
-
-
-
-};
-
-  reader.readAsText(file);
-}
-
-// smaller graph practice
-function handleSmallFile() {
-  // read the csv file after taking it as input
-  var fileInput = document.getElementById("smallCsvFileInput");
-  var file = fileInput.files[0];
-  var reader = new FileReader();
-
-  reader.onload = function (e) {
-    var contents = e.target.result;
-    var lines = contents.split("\n");
-    var dataByHeaders = {};
-
-// get header values
-    var headers = lines[0].split(",");
-
-// Initialize dataByHeaders object (sorting data by headers)
-    for (var i = 1; i < headers.length; i++) {
-      dataByHeaders[headers[i]] = {};
-    }
-
-// parse through each line of data
-    for (var i = 1; i < lines.length; i++) {
-      var line = lines[i].split(",");
-
-  // Assign values to the headers
-      for (var j = 1; j < headers.length; j++) {
-        var xHeader = headers[j];
-        var yHeader = line[0];
-        var value = parseInt(line[j]);
-
-        if (!isNaN(value)) {
-          if (!dataByHeaders[xHeader].hasOwnProperty(yHeader)) {
-            dataByHeaders[xHeader][yHeader] = 0;
-          }
-
-          dataByHeaders[xHeader][yHeader] += value;
-        }
-      }
-    }
-    var totalValue = 0;
-    for (var xHeader in dataByHeaders) {
-      console.log(xHeader + ":");
-      var yHeaders = Object.keys(dataByHeaders[xHeader]);
-
-      for (var yHeader of yHeaders) {
-        var value = dataByHeaders[xHeader][yHeader];
-        console.log(yHeader + "\nValue: " + value);
-        totalValue += value;
-      }
-
-      console.log("------");
-      console.log("Total Value: " + totalValue);
-    }
-var barData = [];
-
-var colorIndex = 0;
-// parse through data
-for (var xHeader in dataByHeaders) {
-  var yHeaders = Object.keys(dataByHeaders[xHeader]);
-  var values = [];
-  for (var xHeader in dataByHeaders) {
-    var yHeaders = Object.keys(dataByHeaders[xHeader]);
-    var values = [];
-    for (var yHeader of yHeaders) {
-      var value = dataByHeaders[xHeader][yHeader];
-      values.push(value);
-    }
-    //sort data, then flip it since smallest comes before largest when sorting
-    var sortedIndices = values.map(function (_, index) {
-      return index;
-    }).sort(function (a, b) {
-      return values[b] - values[a];
-    });
-    yHeaders = sortedIndices.map(function (index) {
-      return yHeaders[index];
-    });
-    values = sortedIndices.map(function (index) {
-      return values[index];
-    });
-  }
-  yHeaders.reverse();
-  values.reverse();
-
-  var barTrace = {
-    y: yHeaders,
-    x: values,
-    width: [0.7, 0.7, 0.7, 0.7, 0.7, 0.7, 0.7, 0.7, 0.7, 0.7 
-       ],
-    type: 'bar',
-    name: xHeader,
-    orientation: 'h',
-    text: values.map(String),
-    textposition: 'auto',
-    textfont_size: 20,
-    textposition: 'outside',
-    cliponaxis: false,
-    
-  };
-  barData.push(barTrace);
-  colorIndex++;
-}
-
-var barLayout = createBarChartLayout(headers, values);
-
-Plotly.newPlot('smallBarChartContainer', barData, barLayout).then(function() {
-  var barChart = document.getElementById('smallBarChartContainer');
-  var colors = Array.from({ length: yHeaders.length }, () => 'rgba(31, 119, 180, 0.8)'); // Blue color
-  
-
-  barChart.on('plotly_click', function(data) {
-    var clickedIndex = data.points[0].pointIndex;
-
-    // Reset colors to blue for all bars
-    colors = Array.from({ length: yHeaders.length }, () => 'rgba(31, 119, 180, 0.8)');
-
-    // Set the clicked bar color to green
-    colors[clickedIndex] = 'green';
-
-    var update = {
-      marker: { color: colors }
-    };
-
-    Plotly.update('smallBarChartContainer', update);
-    
-    // showcase the clicked bar's information onto the webpage itself
-    var clickedBarData = data.points[0]; // Get the clicked bar's data directly
-    var clickedBarName = clickedBarData.label;
-    var clickedBarValue = clickedBarData.x;
-    console.log(clickedBarData)
-
-
-    var statement = "Selected Information: " + clickedBarName + "<br>Value: " + clickedBarValue;
-    document.getElementById('outputSelectSmallInfo').innerHTML = statement;
-    document.getElementById('outputSelectSmallInfo').style.border = "2px solid green";
-  });
-});
-  }
   reader.readAsText(file);
 }
